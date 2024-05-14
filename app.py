37f28022d61c488f0420c72ceccbeb4ca3bfba42
RubaAlhajri <158767356+RubaAlhajri@users.noreply.github.com>
2024-05-14 23:27:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ st.image('https://www.ar8ar.com/wp-content/uploads/2022/10/%D8%AC%D8%AF%D8%A7%D8
 st.markdown("<h1 style='color: #a38d62;'>Jadarat</h1>", unsafe_allow_html=True)
 
 st.write("Among the thousands of job postings,  Imagine having the power to navigate through opportunities, discovering the most sought-after skills and emerging trends. These statistics are the key to unlocking possibilities, helping companies fine-tune recruitment strategies and make informed decisions. Whether you're a hiring manager or a job seeker, job posting statistics offer valuable information to guide your journey. So, dive into the data and set sail toward success in the world of work")
+
+st.image('Region.png')
+
+
+st.write("According to Jadarat, most job postings are located in the Riyadh region, secondly in the Makkah Al-Mukarramah region, and thirdly in the Eastern Province. This indicates the abundance of job opportunities in Riyadh, it is the capital of the Kingdom of Saudi Arabia and most of the main headquarters are located there. Its population is high compared to other regions")
