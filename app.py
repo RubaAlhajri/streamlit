7c57eab73800d4c5c011f87c230b23219c6f690f
RubaAlhajri <158767356+RubaAlhajri@users.noreply.github.com>
2024-05-30 21:14:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-
+import streamlit as st
 def display(text, font="sans-serif", color="white", size="25px"):
     st.markdown(
         f'<p style="font-family:{font}; color:{color}; font-size: {size};">{text}</p>',
         unsafe_allow_html=True,
     )
 
-# st.image('https://www.ar8ar.com/wp-content/uploads/2022/10/%D8%AC%D8%AF%D8%A7%D8%B1%D8%A7%D8%AA.png')
+st.image('https://www.ar8ar.com/wp-content/uploads/2022/10/%D8%AC%D8%AF%D8%A7%D8%B1%D8%A7%D8%AA.png')
 st.markdown("<h1 style='color: #a38d62;'>Jadarat</h1>", unsafe_allow_html=True)
 
 st.write("Among the thousands of job postings,  Imagine having the power to navigate through opportunities, discovering the most sought-after skills and emerging trends. These statistics are the key to unlocking possibilities, helping companies fine-tune recruitment strategies and make informed decisions. Whether you're a hiring manager or a job seeker, job posting statistics offer valuable information to guide your journey. So, dive into the data and set sail toward success in the world of work")
